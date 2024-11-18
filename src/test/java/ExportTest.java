d8046f815f03d0ef5b88de6e1bcfc3ac64dadc43
JackBao <153061108+Jack20410@users.noreply.github.com>
2024-11-19 01:06:17 +0700

    change

--- src/test/java/ExportTest.java
+++ src/test/java/ExportTest.java
@@ -3,7 +3,7 @@ import com.tdtu.utils.CSVExporter;
 public class ExportTest {
     public static void main(String[] args) {
         try {
-            CSVExporter.exportStudents("F:\\Java Tech\\Mid_Term\\src\\main\\resources\\exported_students.csv");
+            CSVExporter.exportStudents(ExportTest.class.getResource("/exported_students.csv").getPath());
             System.out.println("Students exported successfully!");
         } catch (Exception e) {
             e.printStackTrace();
