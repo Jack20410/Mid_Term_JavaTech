a0bc7097b497f10c5495d2d385338a2545c41742
JackBao <522k0003@student.tdtu.edu.vn>
2024-11-19 23:52:25 +0700

    fix path

--- src/test/java/ExportTest.java
+++ src/test/java/ExportTest.java
@@ -3,7 +3,7 @@ import com.tdtu.utils.CSVExporter;
 public class ExportTest {
     public static void main(String[] args) {
         try {
-            CSVExporter.exportStudents(ExportTest.class.getResource("/exported_students.csv").getPath());
+            CSVExporter.exportStudents("./src/main/resources/exported_students.csv");
             System.out.println("Students exported successfully!");
         } catch (Exception e) {
             e.printStackTrace();
--- src/test/java/ImportTest.java
+++ src/test/java/ImportTest.java
@@ -4,7 +4,7 @@ public class ImportTest {
     public static void main(String[] args) {
         try {
             // Use class loader to locate the file
-            String filePath = (ImportTest.class.getResource("/sample_students.csv")).getPath();
+            String filePath = ("./src/main/resources/sample_students.csv");
             CSVImporter.importStudents(filePath);
             System.out.println("Students imported successfully!");
         } catch (Exception e) {
